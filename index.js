be6328f1212acd2e65d7f3aaa5af8fb7952683e5
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2024-04-25 13:21:54 -0600

    Simplify script

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var asciify = require('asciify-image');
-
-var options = {
-  fit:    'box',
-  width:  200,
-  height: 100
-}
-
-asciify('https://octodex.github.com/images/octofez.png', options)
-  .then(function (asciified) {
-    // Print asciified image to console
-    console.log(asciified);
-  })
-  .catch(function (err) {
-    // Print error to console
-    console.error(err);
-  });
+console.log('Hello world!')
